9c438bf4f2ab147a361d2a441b081795c342ba57
Maxim <caliber1997@gmail.com>
2018-01-11 01:47:35 +0200

    Update parameter descriptions

--- src/ParametersDialog/index.js
+++ src/ParametersDialog/index.js
@@ -31,27 +31,27 @@ const ParametersDialog = ({ open, onClose, onOpenAlgorithm }) => (
             </TableRow>
             <TableRow>
               <TableCell>Topology</TableCell>
-              <TableCell>The topology of the swarm defines the subset of particles with which each particle can exchange information.</TableCell>
+              <TableCell>Defines the subset of particles with which each particle can exchange information.</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>ω</TableCell>
-              <TableCell>Previous velocity coefficient</TableCell>
+              <TableCell>Previous velocity coefficient (inertia).</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>φ<sub>p</sub></TableCell>
-              <TableCell>Particle best coefficient</TableCell>
+              <TableCell>Particle best coefficient.</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>φ<sub>g</sub></TableCell>
-              <TableCell>Global (or group's) best coefficient</TableCell>
+              <TableCell>Global (or group's) best coefficient.</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>max v.</TableCell>
+              <TableCell>max v</TableCell>
               <TableCell>Maximum velocity of a particle, after which the value is cut.</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Optimization function</TableCell>
-              <TableCell>One of the possible fitness landscapes for optimal value search</TableCell>
+              <TableCell>One of the possible fitness landscapes for optimal value search. (See function descriptions <a href="https://l.messenger.com/l.php?u=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTest_functions_for_optimization%23Test_functions_for_single-objective_optimization&h=ATMi8JaewAm9tRY_N0KPOKiCxqFV_yr_GfoFDMiYPIbdhGkd3MmCUW0Zb5q95JAbFQ1dNGfDD4MWJ3cZMC4bYrEheAf9OhsF5os0Q_66hENgU9LJhLkntHds3oLFx-qWme3RHg">here</a>).</TableCell>
             </TableRow>
           </TableBody>
         </Table>
